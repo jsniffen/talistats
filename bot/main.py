f98cdf334e858947c2d664cfa536af32158779e7
annuvin8951 <jessehinman21@gmail.com>
2025-01-10 12:13:53 -0500

    Fixed Merge

--- bot/main.py
+++ bot/main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import db
 import discord
 import os
+import storage
 import talishar
 import traceback
 import threading
@@ -18,6 +19,13 @@ mutex = threading.Lock()
 queue = []
 waitingLobby = dict()
 activeMatches = dict()
+
+formats = ["cc", "draft", "ll", "sealed", "blitz"]
+
+@bot.event
+async def on_ready():
+    if os.getenv("ENV") == "prod":
+        storage.upload_db.start()
 
 def createSubmitPanel():
     embed = discord.Embed(
@@ -150,14 +158,18 @@ class QueueButtons(discord.ui.View):
 @bot.command()
 async def winrate(ctx, hero: discord.Option(str, autocomplete=db.distinct_heroes)):
     try:
-        winrate = await db.get_winrate(hero)
-        await ctx.respond(f"{hero} winrate: {winrate:.0f}%")
+        winrates = await db.winrates(hero)
+
+        lines = [f"{x['hero']} vs. {x['opponent']}: {x['winrate']:.0f}%" for x in winrates]
+        await ctx.respond("\n".join(lines))
+        print(f"Successfully handled winrate: hero={hero}")
     except:
+        print(f"Failed to handle winrate: hero={hero}")
         print(traceback.format_exc())
         await ctx.respond(f"Error calculating winrate for {hero}")
 
 @bot.command()
-async def report(ctx, match_id: discord.Option(str)):
+async def report(ctx, match_id: discord.Option(str), format: discord.Option(str, default="cc", choices=formats)):
     try:
         match = talishar.get_match_stats(match_id)
 
@@ -172,7 +184,9 @@ async def report(ctx, match_id: discord.Option(str)):
         await db.insert_match(match)
         await ctx.respond(f"Added result: {match.summary()}")
 
+        print(f"Successfully handled report: match_id={match_id} format={format}")
     except Exception:
+        print(f"Failed to handle report: match_id={match_id} format={format}")
         print(traceback.format_exc())
         await ctx.respond("Error retrieving results from talishar")
 
