47c2d5dcb2fbdb36ae8230cb9ad1e42998c11961
Julian Sniffen <julian.sniffen@gmail.com>
2025-01-20 00:23:18 -0500

    upload date db as well

--- bot/storage.py
+++ bot/storage.py
@@ -1,8 +1,10 @@
 from discord.ext import tasks
 from dotenv import load_dotenv
+from datetime import datetime
 import boto3
 import os
 import traceback
+import asyncio 
 
 load_dotenv()
 
@@ -20,9 +22,17 @@ def download_db():
 async def upload_db():
     try:
         s3 = boto3.client("s3", endpoint_url=ENDPOINT, aws_access_key_id=ACCESS_ID, aws_secret_access_key=SECRET_KEY)
+
         s3.upload_file(DB_NAME, SPACE, DB_NAME)
         s3.put_object_acl(ACL='public-read', Bucket=SPACE, Key=DB_NAME)
         print(f"Successfully uploaded {DB_NAME} to {SPACE}")
+
+        date = datetime.today().strftime('%Y_%m_%d')
+        DATE_DB_NAME = f"{date}_{DB_NAME}"
+        s3.upload_file(DB_NAME, SPACE, DATE_DB_NAME)
+        s3.put_object_acl(ACL='public-read', Bucket=SPACE, Key=DATE_DB_NAME)
+        print(f"Successfully uploaded {DATE_DB_NAME} to {SPACE}")
+
     except Exception as e:
         print(f"Failed to upload {DB_NAME} to {SPACE}")
         print(traceback.format_exc())
