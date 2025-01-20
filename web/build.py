a5bb186e24f57d51ba477624bf692f204f1c5e10
Julian Sniffen <julian.sniffen@gmail.com>
2025-01-20 17:53:45 -0500

    fix python syntax error

--- web/build.py
+++ web/build.py
@@ -2,4 +2,4 @@ import os
 
 with open("env.js", "w") as file:
     db_url = os.environ["DB_URL"]
-    file.write(f"""export const DB_URL = "{db_url}";""")
+    file.write(f"export const DB_URL = \"{db_url}\";")
---
+++ web/env-old.js
@@ -0,0 +1,1 @@
+export const DB_URL = "https://talistats-db.nyc3.cdn.digitaloceanspaces.com/talistats-db/talistats.sqlite";
