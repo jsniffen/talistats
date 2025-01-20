d8827e4a3f65a5ae14bc063e1f48202548bbab0d
Julian Sniffen <julian.sniffen@gmail.com>
2025-01-20 17:56:31 -0500

    test this

--- web/build.py
+++ web/build.py
@@ -2,4 +2,5 @@ import os
 
 with open("env.js", "w") as file:
     db_url = os.environ["DB_URL"]
-    file.write(f"export const DB_URL = \"{db_url}\";")
+    print(db_url)
+    # file.write(f"export const DB_URL = \"{db_url}\";")
