62896d6a7067c5d3baf30e38d58d9db8dd777017
Julian Sniffen <julian.sniffen@gmail.com>
2025-01-20 18:23:24 -0500

    try this?

--- web/build.py
+++ web/build.py
@@ -2,5 +2,6 @@ import os
 
 with open("env.js", "w") as file:
     db_url = os.environ["DB_URL"]
-    print(db_url)
-    # file.write(f"export const DB_URL = \"{db_url}\";")
+    file.write("export const DB_URL = \"")
+    file.write(db_url)
+    file.write("\";")
--- web/env-old.js
+++
@@ -1,1 +0,0 @@
-export const DB_URL = "https://talistats-db.nyc3.cdn.digitaloceanspaces.com/talistats-db/talistats.sqlite";
